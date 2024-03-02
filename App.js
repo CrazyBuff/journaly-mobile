462fc015633398bed75440bd6f016cf97e8cf6b5
Renchie Yang <ryan0042@student.monash.edu>
2024-03-02 16:11:35 +1100

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+const image = { uri: 'https://www.eaglecreek.com/cdn/shop/articles/An_20empty_20page_20in_20a_20travel_20journal.jpg' }
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={image} resizeMode='cover' style={styles.image}>
+        <View> 
+
+        </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  image: {
+    flex: 1,
     justifyContent: 'center',
   },
+  loginContainer: {
+    
+  }
 });
